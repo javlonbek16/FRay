ac13b79ea7f4f6cede64327b9160c888bfc259ac
Javlonbek <nematovjavlonbek16@gmail.com>
2023-11-20 10:11:20 +0500

    update

--- app/Http/Repositories/FilterRepository.php
+++ app/Http/Repositories/FilterRepository.php
@@ -12,13 +12,17 @@ class FilterRepository implements FilterInterface
     public function getShows(Request $request)
     {
         try {
-
             $data = Show::with([
                 'artists',
                 'venues',
-
             ])->when($request->start_date, function ($query) use ($request) {
-                $query->orderBy('start_date', 'asc', $request->start_date);
+                $query->orderBy('start_date', $request->start_date == 'desc' ? 'desc' : 'asc');
+            })->when($request->is_complete, function ($query) use ($request) {
+                $query->where('is_complete', $request->is_complete == true ? true : false);
+            })->when($request->artist_name, function ($query)   use ($request) {
+                $query->where('artist_name', 'like', '%' . $request->artist_name . '%');;
+            })->when($request->venue_name, function ($query) use ($request) {
+                $query->where('venue_name', 'like', '%' . $request->venue_name . '%');
             });
             return ShowResource::collection($data);
         } catch (\Exception $e) {
